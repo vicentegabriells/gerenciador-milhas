5ee1a753173d605ad1aee45cb2472731d10c546f
Vicente Souza <vicentesouza.dev@gmail.com>
2025-11-03 21:08:53 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/milhas/gerenciador/repository/UsuarioRepository.java
+++ src/main/java/br/com/milhas/gerenciador/repository/UsuarioRepository.java
@@ -1,15 +1,14 @@
 package br.com.milhas.gerenciador.repository;
 
-import java.util.Optional;
-
+import br.com.milhas.gerenciador.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.milhas.gerenciador.model.Usuario;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    // Spring Data JPA vai criar a consulta automaticamente baseada no nome do método
+    // Método customizado: O Spring cria a consulta para buscar um usuário pelo e-mail
     Optional<Usuario> findByEmail(String email);
 }
